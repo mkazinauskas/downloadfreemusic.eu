977123328dc2bcd94680d35b042ef5e16f75398a
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2018-09-13 22:31:45 +0300

    Better update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM alimashuri/alpine-apache-php5
-COPY html/ /web/html
 RUN sed -i 's/AllowOverride None/AllowOverride All/g' /etc/apache2/httpd.conf \
  && echo "LoadModule rewrite_module modules/mod_rewrite.so" >> /etc/apache2/httpd.conf
--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ docker kill downloadfreemusic.eu
 docker rm downloadfreemusic.eu
 docker rmi downloadfreemusic.eu
 docker build -t downloadfreemusic.eu .
-docker run -p 5001:80 -d --name downloadfreemusic.eu downloadfreemusic.eu
+docker run -p 5001:80 --mount type=bind,source="$(pwd)"/html,target=/web/html,readonly -d --name downloadfreemusic.eu downloadfreemusic.eu
 #docker save downloadfreemusic.eu > downloadfreemusic.eu.tar
